5f520916a13311d00fc6fb58d7b7628fde382e3a
kw7oe <choongkwern@hotmail.com>
2022-05-23 08:10:28 +0800

    WIP support multiple client

--- src/client.rs
+++ src/client.rs
@@ -1,15 +1,46 @@
+use bytes::Buf;
+use std::error::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt as _};
 use tokio::net::TcpStream;
 mod packet;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let domain = "test.rok.me".to_string();
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let domain = init().await?;
     if let Err(e) = run_data_channel(domain).await {
         println!("{:?}", e);
     }
 
     Ok(())
+}
+
+async fn init() -> Result<String, Box<dyn Error + Send + Sync>> {
+    let mut cc = TcpStream::connect("rok.me:3001").await?;
+    let mut buf = bytes::BytesMut::with_capacity(1024);
+
+    // Send a Init
+    let init = packet::Packet::Init;
+    cc.write_all(&bincode::serialize(&init).unwrap()).await?;
+    let len = cc.read_buf(&mut buf).await?;
+    let domain = if let packet::Packet::Success(domain) = packet::Packet::parse(&buf) {
+        println!("tunnel up!\nHost: {domain}");
+        Some(domain)
+    } else {
+        None
+    };
+    buf.advance(len);
+
+    if domain.is_none() {
+        return Err("fail to init with server".into());
+    }
+
+    // Let tunnel know client is ready
+    cc.write_all(&bincode::serialize(&packet::Packet::Ack).unwrap())
+        .await?;
+
+    println!("control channel established!");
+
+    domain.ok_or_else(|| "no domain return".into())
 }
 
 async fn run_data_channel(domain: String) -> std::io::Result<()> {
--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,78 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::RwLock;
+use tokio::sync::{Mutex, RwLock};
 
 mod packet;
 
 type State = Arc<RwLock<HashMap<String, TcpStream>>>;
+type DomainPort = (String, u16);
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
     let listener = TcpListener::bind("127.0.0.1:3001").await?;
+    let domain_to_port = Arc::new(Mutex::new(vec![
+        ("test.rok.me".to_string(), 3002),
+        ("test2.rok.me".to_string(), 3003),
+        ("test3.rok.me".to_string(), 3004),
+    ]));
+
     let state: State = Arc::new(RwLock::new(HashMap::new()));
     println!("Listening on TCP: 127.0.0.1:3001");
     loop {
         if let Ok((conn, _)) = listener.accept().await {
             println!("Accpeting new client...");
             let state = state.clone();
+            let domains = domain_to_port.clone();
             tokio::spawn(async move {
-                let _ = handle_connection(conn, state).await;
+                let _ = handle_connection(conn, domains, state).await;
             });
         }
     }
 }
 
-async fn handle_connection(mut conn: TcpStream, state: State) -> std::io::Result<()> {
+async fn handle_connection(
+    mut conn: TcpStream,
+    domains: Arc<Mutex<Vec<DomainPort>>>,
+    state: State,
+) -> std::io::Result<()> {
     let mut buffer = BytesMut::with_capacity(4096);
 
     let bytes_len = conn.read_buf(&mut buffer).await?;
     let packet = packet::Packet::parse(&buffer);
     match packet {
+        packet::Packet::Init => {
+            let mut domains = domains.lock().await;
+
+            let domain_port = domains.pop();
+
+            if domain_port.is_none() {
+                println!("oops no more domain available, ignore you");
+                return Ok(());
+            }
+
+            let domain_port = domain_port.unwrap();
+            let domain = domain_port.0;
+            let success = packet::Packet::Success(domain.clone());
+            conn.write_all(&bincode::serialize(&success).unwrap())
+                .await?;
+            println!("sent success msg");
+
+            buffer.advance(bytes_len);
+            let len = conn.read_buf(&mut buffer).await;
+            if let packet::Packet::Ack = packet::Packet::parse(&buffer) {
+                println!("receive ack from client");
+
+                let mut state = state.write().await;
+
+                // Ask client to create a data channel.
+                // let create_data = &bincode::serialize(&packet::Packet::CreateData).unwrap();
+                // conn.write_all(create_data).await?;
+                // println!("sent create data msg");
+                state.insert(domain, conn);
+            }
+        }
         packet::Packet::DataInit(_domain) => {
             let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
             println!("Listening to 127.0.0.1:3000");
